919428f0d3f4ce21753dce9d2f192f1017fe06b6
Check <farid.m.check@gmail.com>
2017-07-20 08:07:18 +0200

    Suppression/renommage du dossier 'Interface' en Interfaces (c'etait une appelation reservée).

---
+++ src/AppBundle/Interfaces/Admin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smael
+ * Date: 20/07/2017
+ * Time: 08:05
+ */
+
+namespace AppBundle\Interfaces;
+
+interface Admin{
+
+}
---
+++ src/AppBundle/Interfaces/Secretaire.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smael
+ * Date: 20/07/2017
+ * Time: 08:03
+ */
+namespace AppBundle\Interfaces;
+
+interface Secretaire{
+
+}
